349b8721c7a215cbbe4e1ff7993a52679114734c
iOS-mikedilulo <saintmichael0710@gmail.com>
2020-04-10 13:55:33 -0600

    Return next number of integer passed

--- app.veryEasy.swift
+++ app.veryEasy.swift
@@ -1,9 +1,14 @@
-// return the sum of two numbers 
+// Return the sum of two numbers 
 func addition(_ a: Int, _ b: Int) -> Int {
     return a + b
 }
 
-// area of a triangle
+// Area of a triangle
 func areaOfTriangle (_ base: Int, _ height: Int) -> Int {
     return base * height / 2
+}
+
+// Return the next number of an integer passed
+func nextNumberPassed(_ num: Int) -> Int {
+    return num + 1
 }
